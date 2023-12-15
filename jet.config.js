4e198aeca0648dab577f7dba93c3c7edb6b9ca98
aymen04 <125768153+aymen050@users.noreply.github.com>
2023-12-15 20:09:34 +0100

    test

---
+++ jet.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    // Options de configuration Jest personnalisées ici
+    // Par exemple :
+    testMatch: ["**/tests/unit/*.js"], // Chemin des fichiers de test Jest
+    transform: {
+      "^.+\\.js$": "babel-jest", // Transformateur pour les fichiers JS
+    },
+  };
+  
--- tests/unit/unit-test.test.mjs
+++ tests/unit/unit-test.test.mjs
@@ -1,26 +1,15 @@
- 
+import { test, expect } from '@playwright/test';
+
+// Vos tests ici...
+
+
 function add(a, b) {
+  return a + b;
+}
 
-    return a + b;
-
-  }
-
-  test('Test d\'addition', () => {
-
-    // Arrange
-
-    const num1 = 2;
-
-    const num2 = 3;
-
-    // Act
-
-    const result = add(num1, num2);
-
-    // Assert
-
-    expect(result).toBe(5);
-
-  });
-
- 
+test('Test d\'addition', () => {
+  const num1 = 2;
+  const num2 = 3;
+  const result = add(num1, num2);
+  expect(result).toBe(5);
+});
